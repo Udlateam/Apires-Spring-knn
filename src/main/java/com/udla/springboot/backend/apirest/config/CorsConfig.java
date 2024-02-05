f1401d547cf095a5052fe792c8352786d336792a
Udlateam <tiorpecuadorbbb@gmail.com>
2024-02-04 20:25:15 -0500

    Configurando cores para react

--- src/main/java/com/udla/springboot/backend/apirest/config/CorsConfig.java
+++ src/main/java/com/udla/springboot/backend/apirest/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://proyectoangular.vercel.app")
+                        .allowedOrigins("http://localhost:4200", "https://proyectoangular.vercel.app","http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
