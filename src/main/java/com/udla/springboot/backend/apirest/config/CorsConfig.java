47cae2005ea3001b6ef8cc388dec695ff24ea419
Udlateam <tiorpecuadorbbb@gmail.com>
2024-02-04 21:08:09 -0500

    Configurando DEPLOY cores para react

--- src/main/java/com/udla/springboot/backend/apirest/config/CorsConfig.java
+++ src/main/java/com/udla/springboot/backend/apirest/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://proyectoangular.vercel.app","http://localhost:5173","https://5173-randymejiaa-featurefron-pgcemnb0lg0.ws-us108.gitpod.io")
+                        .allowedOrigins("http://localhost:4200", "https://proyectoangular.vercel.app","http://localhost:5173","https://5173-randymejiaa-featurefron-pgcemnb0lg0.ws-us108.gitpod.io","https://zingy-torrone-d96483.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
